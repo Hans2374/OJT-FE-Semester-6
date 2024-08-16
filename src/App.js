73e28b83beb2ec18c069fdf96249b9571d9887ec
HotMilk1411 <tranvangiabao2002@gmail.com>
2024-08-16 09:47:44 +0700

    ACFHL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
