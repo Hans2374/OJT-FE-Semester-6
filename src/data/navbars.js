d29d112b4ec9313e97ac7f9753fa923b1e1118bf
HotMilk1411 <tranvangiabao2002@gmail.com>
2024-08-17 17:56:15 +0700

    nav update

--- src/data/navbars.js
+++ src/data/navbars.js
@@ -4,7 +4,7 @@ const topNav = [
     {id:1, label:'Home', href:'#'},
     {id:2, label:'About', href:'#'},
     {id:3, label:'Share', href:'#'},
-    {id:4, label:'Logout', href:'#'>},
+    {id:4, label:'Logout', href:'#'},
 ];
 
 export const getTopNav = () => {
