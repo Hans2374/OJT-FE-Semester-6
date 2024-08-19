e1c6c420adc1c316dd3ee5155dba845978632da4
HotMilk1411 <tranvangiabao2002@gmail.com>
2024-08-19 17:31:13 +0700

    update Login Roles

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -8,8 +8,32 @@ import { login } from "../../features/userSlice";
 const Login = () => {
   const [username,setUsername] = useState("");
   const [password,setPassword] = useState("");
-
+  const [roles, setRoles] = useState([]);
   const dispatch = useDispatch();
+
+  const checkBoxInputs = [
+    {
+      id: 1,
+      label: "Admin",
+      value: "admin"
+    },
+    {
+      id: 2,
+      label: "Intern",
+      value: "intern"
+    }
+  ];
+
+  const [checkedvalue, setCheckedValue] = useState();
+
+  const handleCheckboxChange = (e) => {
+    if (e.target.checked) {
+      setCheckedValue(e.target.value);
+      if (!roles.includes(e.target.value)) {
+        setRoles([...roles, e.target.value]);
+      }
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,6 +42,7 @@ const Login = () => {
       login({
       name:username,
       password:password,
+      role: roles,
       loggedIn: true,
       })
     );
@@ -25,17 +50,57 @@ const Login = () => {
 
   return (
     <div className="wrapper">
-      <form className="form-group custom-form" onSubmit={(e) => handleSubmit(e)}>
+      <form
+        className="form-group custom-form"
+        onSubmit={(e) => handleSubmit(e)}
+      >
         <h1>Login</h1>
         <div className="input-box">
-          <input value={username} onChange={(e) => setUsername(e.target.value)} type="name" required placeholder="Username" />
+          <input
+            autocomplete="off"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            type="name"
+            required
+            placeholder="Username"
+          />
         </div>
 
         <div className="input-box">
-          <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" required placeholder="Password" />
+          <input
+            autocomplete="off"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            required
+            placeholder="Password"
+          />
         </div>
 
-        <button type="submit" className="btn btn-primary">LOGIN</button>
+        <div>
+          <div className="change-role">Choose user roles:</div>
+          <ul>
+            {checkBoxInputs.map((role, index) => (
+              <li key={index}>
+                <input
+                  type="checkbox"
+                  id={role.value}
+                  name="roles"
+                  value={role.value}
+                  checked={role.value === checkedvalue}
+                  onChange={handleCheckboxChange}         
+                />
+                <label htmlFor={role.value} className="text-sm ml-1">
+                  {role.label}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          LOGIN
+        </button>
       </form>
     </div>
   );
