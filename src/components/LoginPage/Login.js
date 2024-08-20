13915b9cb98a25fd171df3a1dc5a5b99ca9f30b5
HotMilk1411 <tranvangiabao2002@gmail.com>
2024-08-20 16:36:45 +0700

    Final Login styke commit

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
 
   const checkBoxInputs = [
     { id: 1, label: "Admin", value: "admin" },
-    { id: 2, label: "Intern", value: "intern" }
+    { id: 2, label: "Intern", value: "intern" },
   ];
 
   const [checkedValue, setCheckedValue] = useState();
@@ -42,62 +42,69 @@ const Login = () => {
   };
 
   return (
-    <div className="wrapper">
-      <form className="form-group custom-form" onSubmit={handleSubmit}>
-        <h1 className="underline">Sign In</h1>
-        <div className="checkbox-group">
-          {checkBoxInputs.map((role, index) => (
-            <div className="checkbox" key={index}>
-              <label className="checkbox-wrapper">
-                <input
-                  type="checkbox"
-                  className="checkbox-input"
-                  id={role.value}
-                  name="roles"
-                  value={role.value}
-                  checked={role.value === checkedValue}
-                  onChange={handleCheckboxChange}
-                />
-                <span className="checkbox-tile">
-                  <span className="checkbox-icon">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 101 101" id="user"><path d="M50.4 54.5c10.1 0 18.2-8.2 18.2-18.2S60.5 18 50.4 18s-18.2 8.2-18.2 18.2 8.1 18.3 18.2 18.3zm0-31.7c7.4 0 13.4 6 13.4 13.4s-6 13.4-13.4 13.4S37 43.7 37 36.3s6-13.5 13.4-13.5zM18.8 83h63.4c1.3 0 2.4-1.1 2.4-2.4 0-12.6-10.3-22.9-22.9-22.9H39.3c-12.6 0-22.9 10.3-22.9 22.9 0 1.3 1.1 2.4 2.4 2.4zm20.5-20.5h22.4c9.2 0 16.7 6.8 17.9 15.7H21.4c1.2-8.9 8.7-15.7 17.9-15.7z"></path></svg>
+      <div className="wrapper">
+        <form className="form-group custom-form" onSubmit={handleSubmit}>
+          <h1 className="underline">Sign In</h1>
+          <div className="checkbox-group">
+            {checkBoxInputs.map((role, index) => (
+              <div className="checkbox" key={index}>
+                <label className="checkbox-wrapper">
+                  <input
+                    type="checkbox"
+                    className="checkbox-input"
+                    id={role.value}
+                    name="roles"
+                    value={role.value}
+                    checked={role.value === checkedValue}
+                    onChange={handleCheckboxChange}
+                    required={checkedValue ? false : true}
+                  />
+                  <span className="checkbox-tile">
+                    <span className="checkbox-icon">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 101 101"
+                        id="user"
+                      >
+                        <path d="M50.4 54.5c10.1 0 18.2-8.2 18.2-18.2S60.5 18 50.4 18s-18.2 8.2-18.2 18.2 8.1 18.3 18.2 18.3zm0-31.7c7.4 0 13.4 6 13.4 13.4s-6 13.4-13.4 13.4S37 43.7 37 36.3s6-13.5 13.4-13.5zM18.8 83h63.4c1.3 0 2.4-1.1 2.4-2.4 0-12.6-10.3-22.9-22.9-22.9H39.3c-12.6 0-22.9 10.3-22.9 22.9 0 1.3 1.1 2.4 2.4 2.4zm20.5-20.5h22.4c9.2 0 16.7 6.8 17.9 15.7H21.4c1.2-8.9 8.7-15.7 17.9-15.7z"></path>
+                      </svg>
+                    </span>
+                    <span className="checkbox-label">{role.label}</span>
                   </span>
-                  <span className="checkbox-label">{role.label}</span>
-                </span>
-              </label>
-            </div>
-          ))}
-        </div>
+                </label>
+              </div>
+            ))}
+          </div>
 
-        <label>USERNAME</label>
-        <div className="input-container">
-          <input
-            autoComplete="off"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            type="text"
-            required
-            placeholder="Username"
-          />
-        </div>
+          <label>USERNAME</label>
+          <div className="input-container">
+            <input
+              autoComplete="off"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              type="text"
+              required
+              placeholder="Username"
+            />
+          </div>
 
-        <label>PASSWORD</label>
-        <div className="input-container">
-          <input
-            autoComplete="off"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            required
-            placeholder="Password"
-          />
-        </div>
+          <label>PASSWORD</label>
+          <div className="input-container">
+            <input
+              autoComplete="off"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              required
+              placeholder="Password"
+            />
+          </div>
 
-        <button type="submit" className="login-btn">
-          Login
-        </button>
-      </form>
-    </div>
+          <button type="submit" className="login-btn">
+            Login
+          </button>
+        </form>
+      </div>
   );
 };
 
